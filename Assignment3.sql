45987b57b8aa8b5e53229710c97df52324733e91
Will <billlach1995@yahoo.com>
2021-09-28 15:03:05 -0400

    Update Assignment3.sql
    Adding correct answers provided by instructor for reference in future use-cases.

--- Assignment3.sql
+++ Assignment3.sql
@@ -13,6 +13,10 @@ WHERE City IN(
 	SELECT City
 	FROM Employees
 )
+--1. CORRECT:
+select distinct city 
+from Customers 
+where city in (select city from Employees)
 
 --2a.
 SELECT DISTINCT City
@@ -21,20 +25,38 @@ WHERE City NOT IN(
 	SELECT City
 	FROM Employees
 )
+--2a. CORRECT:
+select distinct city  
+from Customers 
+where City not in (select distinct city from employees where city is not null)
+
 --b.
 SELECT DISTINCT c.City
 FROM Customers c LEFT OUTER JOIN Employees e ON c.City = e.City
+--b. CORRECT:
+select distinct city from Customers  
+except 
+select distinct city from Employees
 
 --3.
 SELECT p.ProductName, SUM(od.Quantity) AS TotalQuantity
 FROM Products p JOIN [Order Details] od ON p.ProductID = od.ProductID
 GROUP BY p.ProductName;
+--3. CORRECT:
+select ProductID,SUM(Quantity) as QunatityOrdered 
+from [order details]
+group by ProductID
 
 --4.
 SELECT c.City, SUM(od.Quantity) AS QuantityOrdered
 FROM [Order Details] od JOIN Orders o ON od.OrderID = o.OrderID JOIN Customers c ON o.ShipCity = c.City
 GROUP BY c.City
 ORDER BY SUM(od.Quantity) DESC
+--4. CORRECT:
+select city,sum(Quantity) as TotalQty 
+from orders o join [order details] od on o.orderid=od.orderid join customers c on c.customerid=o.CustomerID
+group by city
+
 --5a.
 SELECT DISTINCT City
 FROM Customers
@@ -43,6 +65,16 @@ UNION
 SELECT DISTINCT CITY
 FROM Customers
 HAVING COUNT(City) > 1
+--5a. CORRECT:
+select city from Customers
+except
+select city from customers
+group by city
+having COUNT(*)=1
+union 
+select city from customers
+group by city
+having COUNT(*)=0
 
 --b.
 SELECT DISTINCT City
@@ -53,6 +85,12 @@ FROM Customers
 GROUP BY City
 HAVING COUNT(City) > 2
 )
+--b. CORRECT:
+select city 
+from customers 
+group by city 
+having COUNT(*)>=2
+
 --c.(Using neither subquery or union
 SELECT DISTINCT City
 FROM Customers
@@ -68,15 +106,29 @@ WHERE City IN(
 	GROUP BY ShipCity
 	HAVING COUNT(*) > 2
 )
+--6. CORRECT:
+select distinct city 
+from orders o join [order details] od on o.orderid=od.orderid join customers c on c.customerid=o.CustomerID
+group by city,ProductID
+having COUNT(*)>=2
+
 --7.
 SELECT DISTINCT c.ContactName, c.City, o.ShipCity
 FROM Customers c JOIN Orders o ON c.CustomerID = o.CustomerID
 WHERE c.City != o.ShipCity
+--7. CORRECT:
+select distinct c.CustomerID 
+from orders o join [order details] od on o.orderid=od.orderid join customers c on c.customerid=o.CustomerID
+where City <> ShipCity
 
 --8.
 SELECT TOP 5 SUM(od.Quantity), p.ProductID, p.ProductName, AVG(od.UnitPrice) AS AvgPrice
 FROM Products p JOIN [Order Details] od ON p.ProductID = od.ProductID
 GROUP BY p.ProductID, p.ProductName
+--8. CORRECT:
+select top 5 ProductID,AVG(UnitPrice) as AvgPrice,(select top 1 City from Customers c join Orders o on o.CustomerID=c.CustomerID join [Order Details] od2 on od2.OrderID=o.OrderID where od2.ProductID=od1.ProductID group by city order by SUM(Quantity) desc) as City from [Order Details] od1
+group by ProductID 
+order by sum(Quantity) desc
 
 --9a.
 SELECT DISTINCT City
@@ -85,19 +137,34 @@ WHERE City NOT IN(
 	SELECT ShipCity
 	FROM Orders
 )
+--CORRECT:
+select distinct City from Employees where city not in (select ShipCity from Orders where ShipCity is not null)
+
 --b.
 SELECT DISTINCT e.City
 FROM Employees e LEFT OUTER JOIN Orders o ON e.City = o.ShipCity
 WHERE o.ShipCity IS NULL
+--CORRECT:
+select distinct City from Employees where City is not null except (select ShipCity from Orders where ShipCity is not null)
 
 --10.
 --join orders and employee, subquery city in 
 SELECT City
 FROM 
+--CORRECT:
+select (select top 1 City from Orders o join [Order Details] od on o.OrderID=od.OrderID join Employees e on e.EmployeeID = o.EmployeeID
+group by e.EmployeeID,e.City
+order by COUNT(*) desc) as MostOrderedCity,
+(select top 1 City from Orders o join [Order Details] od on o.OrderID=od.OrderID join Employees e on e.EmployeeID = o.EmployeeID
+group by e.EmployeeID,e.City
+order by sum(Quantity) desc) as MostQunatitySoldCity
+
 --11.
 --DELETE *
 --FROM table
 --HAVING COUNT(*) > 1
+--CORRECT:
+use group by and count(*), if count(*)>1 then delete the rows using sub query
 
 --12.
 CREATE TABLE Employee(
@@ -116,14 +183,21 @@ WHERE empid NOT IN(
 	SELECT mgrid
 	FROM Employee
 )
+--CORRECT:
+select empid from Employee except select mgrid from Employee
 
 --13.
 SELECT TOP COUNT(e.deptid), d.deptname
 FROM Employee e JOIN Dept d ON e.deptid = d.deptid
+--13. CORRECT:
+select deptid from employee group by deptid having count(*) = (select top 1 count(*) from employee group by deptid order by count(*) desc)
 
 --14.
 SELECT TOP 3 MAX(e.salary), d.deptname, e.empid
 FROM Employee e JOIN Dept d ON e.deptid = d.deptid
 GROUP BY d.deptname, e.empid
 ORDER BY MAX(e.salary) DESC
+--14. CORRECT:
+select top 3 deptname,empid,salary from employee e join dep d on e.deptid=d.deptid order by salary,deptname,empid desc
+
 
